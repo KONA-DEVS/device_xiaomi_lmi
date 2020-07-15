d0492d9217dba77e7fce8455649a26871b513a7c
Demon000 <demonsingur@gmail.com>
2020-07-15 22:46:31 +0300

    Imi: add recovery scripts
     * wait for bootdevice
     * setup ADB
     * symlink logical partitions
    
    Change-Id: I78bdab01eb24464ef9416f35ea8e3c50cfef7864
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -9,3 +9,8 @@ $(call inherit-product, vendor/xiaomi/lmi/lmi-vendor.mk)
 
 # Vendor properties
 -include $(LOCAL_PATH)/device_props.mk
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/rootdir/bin/init.recovery.qcom.sh:recovery/root/init.recovery.qcom.sh
