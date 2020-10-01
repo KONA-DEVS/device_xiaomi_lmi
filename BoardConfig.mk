3f4eb50e87a2153b37b85c92f5189c2d868dbbe1
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-09-30 19:20:31 -0500

    lmi: BoardConfig: enable some device specific features
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,8 +10,39 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 # Assert
 TARGET_OTA_ASSERT_DEVICE := lmi
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a75
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a75
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
+
+TARGET_USES_64_BIT_BINDER := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := kona
+TARGET_NO_BOOTLOADER := true
+
 # Kernel
-TARGET_KERNEL_CONFIG := lmi_defconfig
+BOARD_KERNEL_BASE := 0x0000
+BOARD_RAMDISK_OFFSET := 0x1000000
+TARGET_KERNEL_CONFIG := vendor/lineage_lmi_defconfig
+
+# Platform
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno650
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/lmi/BoardConfigVendor.mk
