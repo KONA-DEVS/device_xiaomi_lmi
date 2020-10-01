b9ceb9e166291551f67e2d5560bef8b04773fea8
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-10-01 15:52:29 -0500

    lmi: fix typos

--- lineage_lmi.mk
+++ lineage_lmi.mk
@@ -9,16 +9,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from cmi device
-$(call inherit-product, device/xiaomi/imi/device.mk)
+$(call inherit-product, device/xiaomi/lmi/device.mk)
 
 # Inherit some common MoKee stuff.
 $(call inherit-product, vendor/linege/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_imi
-PRODUCT_DEVICE := imi
+PRODUCT_NAME := lineage_lmi
+PRODUCT_DEVICE := lmi
 PRODUCT_BRAND := Pocophone
-PRODUCT_MODEL := PocophoneF2Pro
+PRODUCT_MODEL := Pocophone F2 Pro
 PRODUCT_MANUFACTURER := Pocophone
 
 BUILD_FINGERPRINT := "qti/qssi/qssi:10/QKQ1.191117.002/V11.0.2.0.QJACNXM:user/release-keys"
