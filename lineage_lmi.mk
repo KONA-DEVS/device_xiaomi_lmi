d4208c156d3ff1fe87fcdd7b4d06fc9b8749739a
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-10-04 21:56:32 +0530

    lmi: update lmi mk
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- lineage_lmi.mk
+++ lineage_lmi.mk
@@ -5,14 +5,10 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from cmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
 # Inherit some common MoKee stuff.
-$(call inherit-product, vendor/linege/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_lmi
