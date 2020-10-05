980f24a5a1d34f9bb9bac54076b7d43fc11dc453
dataoutputstream <mb150996@gmail.com>
2020-10-05 17:21:02 +0200

    Bump fingerprint id

--- lineage_umi.mk
+++ lineage_umi.mk
@@ -23,4 +23,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
- BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys
+ BUILD_FINGERPRINT := google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
