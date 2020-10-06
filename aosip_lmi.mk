e0a611c8124e29e044dcd489de120af96d63fe76
klozz <xXx.Reptar.Rawrr.xXx@gmail.com>
2020-10-06 18:09:10 -0500

    Imi: inherit products the most specific

--- aosip_lmi.mk
+++ aosip_lmi.mk
@@ -5,6 +5,10 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from those products. Most specific first.
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
 # Inherit some common MoKee stuff.
