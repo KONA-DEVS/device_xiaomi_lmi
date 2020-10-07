8e096e90ffbe77cf74d65e20679883258646aa88
dataoutputstream <mb150996@gmail.com>
2020-10-07 12:17:54 +0200

    lmi: Import audio policy and build tinymix
    Change-Id: I3ebce4461092990f496c2b02a372c0adeabce840
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -15,11 +15,12 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    audio.a2dp.default \
+    libaacwrapper \
+    tinymix
+
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/audio/audio_policy_engine_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_engine_configuration.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
