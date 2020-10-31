0f4dfa472674ba9c216e9620a54f46ea84c2c254
TH779 <tongsj2333@gmail.com>
2020-10-31 09:53:49 +0800

    lmi: Add Redmi K30 Pro Zoom Edition variant
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_USES_64_BIT_BINDER := true
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := lmi
+TARGET_OTA_ASSERT_DEVICE := lmi,lmipro
 TARGET_NO_BOOTLOADER := true
 
 # Bootloader
