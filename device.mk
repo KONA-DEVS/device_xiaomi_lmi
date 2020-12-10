15f82e280c95c0a3710fbb8b86cfc3fcc60e6ce5
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-12-10 17:35:34 -0600

    lmi: Build Open Camera instead of Snap Camera
    This because Snap Camera has broken video recording while opencamera not
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -54,7 +54,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
+    OpenCamera
+
+RODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/net.sourceforge.opencamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/net.sourceforge.opencamera.xml
 
 # Configstore
 PRODUCT_PACKAGES += \
