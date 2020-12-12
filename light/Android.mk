raystef66/device_xiaomi_lmi_cr7

lmi: Import passthrough light hal
Change-Id: I21aca4db52179441f065695ca8726832eff01933
Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

light/Android.mk
@@ -1,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.light@2.0-impl.xiaomi_kona
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib64
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_INSTALLED_MODULE_STEM := android.hardware.light@2.0-impl.so
+
+LOCAL_SRC_FILES := Light.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+        libbase \
+        liblog \
+        libhidlbase \
+        libhidltransport \
+        libhardware \
+        libutils \
+        android.hardware.light@2.0
+
+include $(BUILD_SHARED_LIBRARY)

KONA-DEVS/device_xiaomi_lmi

light/Android.mk
@@ -1,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.light@2.0-impl.kona
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib64
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_INSTALLED_MODULE_STEM := android.hardware.light@2.0-impl.so
+
+LOCAL_SRC_FILES := Light.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+        libbase \
+        liblog \
+        libhidlbase \
+        libhidltransport \
+        libhardware \
+        libutils \
+        android.hardware.light@2.0
+
+include $(BUILD_SHARED_LIBRARY)