a439f231fe1b8aa071bf89e3cbc5d802bbc70c64
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-01-08 19:42:41 -0600

    Revert "lmi: Support Alipay and WeChat fingerprint payment"
    This reverts commit 356dfdb3e83e01573fa12176910dd21d737c49d9.

--- device.mk
+++ device.mk
@@ -120,13 +120,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml
 
-# IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
