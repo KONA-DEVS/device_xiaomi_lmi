e473d17fee9a3722e1372120af684fc2dd0ca1f5
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-01-29 07:32:06 +0530

    lmi: make this tree be categorized as aosp tree
    I don't want to promote other roms brand
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/descendant_lmi.mk
+    $(LOCAL_DIR)/aosp_lmi.mk
 
 COMMON_LUNCH_CHOICES := \
-    descendant_lmi-user \
-    descendant_lmi-userdebug \
-    descendant_lmi-eng
+    aosp_lmi-user \
+    aosp_lmi-userdebug \
+    aosp_lmi-eng
