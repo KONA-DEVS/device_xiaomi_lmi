38828cc7fa81310e4f08a9e28101b5efa341b8b9
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-02-10 13:18:58 -0600

    lmi: relocate to vendor
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -91,7 +91,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
 PRODUCT_PACKAGES += \
-    lineage.biometrics.fingerprint.inscreen@1.0-service.lmi
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.lmi
 
 
 # FIXME: master: compat for libprotobuf
