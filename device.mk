bcf23329414e553a0dab1bd844a66a236779a174
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-02-10 12:24:20 -0600

    lmi: drop overlay-remove
    We are going to try to build vendor as much as we can, so we have to drop
    overlay-remove for this proccess
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -162,10 +162,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosp
 
-# overlay-remove
-PRODUCT_PACKAGES += \
-    FrameworksResTarget
-
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
